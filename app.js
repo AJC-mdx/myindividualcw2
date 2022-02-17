568153b01c1292e6677d08198e0ec185f3483ec2
Ahmed Choudhry <AC1953@live.mdx.ac.uk>
2022-02-17 16:31:44 +0000

    added rest api

--- app.js
+++ app.js
@@ -41,3 +41,50 @@ MongoClient.connect(
     db = client.db("lessons-app");
   }
 );
+
+//--rest api--//
+app.param("collectionName", (req, res, next, collectionName) => {
+    req.collection = db.collection(collectionName);
+    return next();
+  });
+  
+  app.get("/", (req, res, next) => {
+    res.send("Select a collection, e.g., /collection/messages");
+  });
+  
+  //GET request
+  app.get("/collection/:collectionName", (req, res, next) => {
+    req.collection.find({}).toArray((e, results) => {
+      if (e) return next(e);
+      res.send(results);
+    });
+  });
+  
+  //POST request
+  app.post("/collection/:collectionName", (req, res, next) => {
+    req.collection.insert(req.body, (e, results) => {
+      if (e) return next(e);
+      res.send(results.ops);
+    });
+  });
+  
+  const ObjectID = require("mongodb").ObjectID;
+  app.get("/collection/:collectionName/:id", (req, res, next) => {
+    req.collection.findOne({ _id: new ObjectID(req.params.id) }, (e, result) => {
+      if (e) return next(e);
+      res.send(result);
+    });
+  });
+  
+  //PUT request
+  app.put("/collection/:collectionName/:id", (req, res, next) => {
+    req.collection.update(
+      { _id: new ObjectID(req.params.id) },
+      { $set: req.body },
+      { safe: true, multi: false },
+      (e, result) => {
+        if (e) return next(e);
+        res.send(result.result.n === 1 ? { msg: "success" } : { msg: "error" });
+      }
+    );
+  });
